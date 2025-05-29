8ef16f762a13fd0e3794f9d72c8bcdce7f58db88
herougo <herougo@gmail.com>
2025-05-28 23:27:42 -0400

    WorkerService1: add install and uninstall logic to Program.cs

--- csharp_projects/WorkerService1/WorkerService1/Program.cs
+++ csharp_projects/WorkerService1/WorkerService1/Program.cs
@@ -1,6 +1,41 @@
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Logging.EventLog;
 using WorkerService1;
+using CliWrap;
+
+const string ServiceName = ".NET Joke Service";
+
+if (args is { Length: 1 })
+{
+    try
+    {
+        string executablePath =
+            Path.Combine(AppContext.BaseDirectory, "WorkerService1.exe");
+
+        if (args[0] is "/Install")
+        {
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "create", ServiceName, $"binPath={executablePath}", "start=auto" })
+                .ExecuteAsync();
+        }
+        else if (args[0] is "/Uninstall")
+        {
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "stop", ServiceName })
+                .ExecuteAsync();
+
+            await Cli.Wrap("sc")
+                .WithArguments(new[] { "delete", ServiceName })
+                .ExecuteAsync();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+
+    return;
+}
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddWindowsService(options =>
