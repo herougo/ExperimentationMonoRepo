99fc14228dd189700776056bfb44fbba3ef32769
herougo <herougo@gmail.com>
2026-01-11 22:01:09 -0500

    CodeParsingExperimentV2: finished FullDependencyGraph build part, added TypeUsageWalker

---
+++ csharp_projects/CodeParsingExperimentV2/CodeParsingNet9/CodeManipulator2/SyntaxWalkers/TypeUsageWalker.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeParsingNet9.CodeManipulator2.SyntaxWalkers
+{
+    // Created with ChatGPT
+
+    internal sealed class TypeUsageWalker : CSharpSyntaxWalker
+    {
+        private readonly SemanticModel _semanticModel;
+        private readonly CancellationToken _cancellationToken;
+
+        private readonly HashSet<INamedTypeSymbol> _results =
+            new(SymbolEqualityComparer.Default);
+
+        public IReadOnlyCollection<INamedTypeSymbol> Results => _results;
+
+        public TypeUsageWalker(
+            SemanticModel semanticModel,
+            CancellationToken cancellationToken)
+            : base(SyntaxWalkerDepth.Node)
+        {
+            _semanticModel = semanticModel;
+            _cancellationToken = cancellationToken;
+        }
+
+        public override void VisitIdentifierName(IdentifierNameSyntax node)
+        {
+            TryAddType(node);
+            base.VisitIdentifierName(node);
+        }
+
+        public override void VisitGenericName(GenericNameSyntax node)
+        {
+            TryAddType(node);
+            base.VisitGenericName(node);
+        }
+
+        public override void VisitQualifiedName(QualifiedNameSyntax node)
+        {
+            TryAddType(node);
+            base.VisitQualifiedName(node);
+        }
+
+        public override void VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
+        {
+            TryAddType(node.Type);
+            base.VisitObjectCreationExpression(node);
+        }
+
+        public override void VisitVariableDeclaration(VariableDeclarationSyntax node)
+        {
+            TryAddType(node.Type);
+            base.VisitVariableDeclaration(node);
+        }
+
+        public override void VisitParameter(ParameterSyntax node)
+        {
+            TryAddType(node.Type);
+            base.VisitParameter(node);
+        }
+
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            TryAddType(node.ReturnType);
+            base.VisitMethodDeclaration(node);
+        }
+
+        private void TryAddType(SyntaxNode? node)
+        {
+            if (node == null)
+                return;
+
+            var symbol = _semanticModel.GetSymbolInfo(node, _cancellationToken).Symbol
+                      ?? _semanticModel.GetTypeInfo(node, _cancellationToken).Type;
+
+            if (symbol is INamedTypeSymbol namedType &&
+                (namedType.TypeKind == TypeKind.Class ||
+                 namedType.TypeKind == TypeKind.Enum ||
+                 namedType.TypeKind == TypeKind.Interface))
+            {
+                _results.Add(namedType);
+            }
+        }
+    }
+}
--- csharp_projects/CodeParsingExperimentV2/CodeParsingNet9/Graphs/FullDependency/FullDependencyGraph.BuildTypeArcs.cs
+++ csharp_projects/CodeParsingExperimentV2/CodeParsingNet9/Graphs/FullDependency/FullDependencyGraph.BuildTypeArcs.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using CodeParsingNet9.CodeManipulator2.SyntaxWalkers;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeParsingNet9.Graphs.FullDependency
 {
@@ -29,8 +29,8 @@ namespace CodeParsingNet9.Graphs.FullDependency
                     var semanticModel = await document.GetSemanticModelAsync();
                     if (semanticModel == null) continue;
 
+                    // methods
                     var methods = syntaxRoot.DescendantNodes().OfType<MethodDeclarationSyntax>();
-
                     foreach (var methodDecl in methods)
                     {
                         var methodSymbol = semanticModel.GetDeclaredSymbol(methodDecl) as IMethodSymbol;
@@ -38,10 +38,44 @@ namespace CodeParsingNet9.Graphs.FullDependency
 
                         var callerNode = GetNode(methodSymbol);
 
-                        .....
+                        var walker = new TypeUsageWalker(semanticModel, default);
+                        walker.Visit(methodDecl);
 
+                        foreach (ISymbol type in walker.Results)
+                        {
+                            var typeNode = GetOrAddNode(type);
+
+                            AddDirectedEdgeIfMissing(typeNode, callerNode, CodeBlockArcType.TypeUsage);
+                        }
                     }
 
+                    // fields
+                    var fields = syntaxRoot.DescendantNodes().OfType<FieldDeclarationSyntax>();
+                    foreach (var fieldDecl in fields)
+                    {
+                        var fieldSymbol = semanticModel.GetDeclaredSymbol(fieldDecl) as IFieldSymbol;
+                        if (fieldSymbol == null) continue;
+
+                        var fieldNode = GetNode(fieldSymbol);
+
+                        var typeNode = GetOrAddNode(fieldSymbol.Type);
+
+                        AddDirectedEdgeIfMissing(typeNode, fieldNode, CodeBlockArcType.TypeUsage);
+                    }
+
+                    // properties
+                    var properties = syntaxRoot.DescendantNodes().OfType<PropertyDeclarationSyntax>();
+                    foreach (var propertyDecl in properties)
+                    {
+                        var propertySymbol = semanticModel.GetDeclaredSymbol(propertyDecl) as IFieldSymbol;
+                        if (propertySymbol == null) continue;
+
+                        var propertyNode = GetNode(propertySymbol);
+
+                        var typeNode = GetOrAddNode(propertySymbol.Type);
+
+                        AddDirectedEdgeIfMissing(typeNode, propertyNode, CodeBlockArcType.TypeUsage);
+                    }
                 }
             }
         }
